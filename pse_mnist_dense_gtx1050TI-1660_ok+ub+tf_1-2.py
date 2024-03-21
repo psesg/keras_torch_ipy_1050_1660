188e948d6d36100e6a6e584d88f53a91b962005d
PanarinSE <pseesp@mail.ru>
2024-03-21 20:10:04 +0300

    supress warning

--- pse_mnist_dense_gtx1050TI-1660_ok+ub+tf_1-2.py
+++ pse_mnist_dense_gtx1050TI-1660_ok+ub+tf_1-2.py
@@ -1,3 +1,11 @@
+import os
+# for Tensorflow Suppressing Warning messages
+''' TF_CPP_MIN_LOG_LEVEL
+0 = all messages are logged (default behavior)
+1 = INFO messages are not printed
+2 = INFO and WARNING messages are not printed
+3 = INFO, WARNING, and ERROR messages are not printed '''
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 
 def setup_tf_GPU (str_tf_ver):
